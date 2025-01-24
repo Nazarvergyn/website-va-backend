79edfe1d40476982cff8d0da8aae349b0cdb7c46
Nazarvergyn <nazarvergyn1234@gmail.com>
2025-01-24 14:09:18 +0100

    Fix server configuration and update models

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
- 
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register({ strapi }) {
+    strapi.server.routes([
+      {
+        method: 'GET',
+        path: '/healthz',
+        handler: (ctx) => {
+          ctx.body = 'OK';
+        },
+        config: {
+          auth: false,
+        },
+      },
+    ]);
+  },
+}; 
